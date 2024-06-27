38aa20b5bc57f222ccd85edcbe8684e9c2730b3f
jarmoza <jarmoza@gmail.com>
2024-06-27 11:35:04 -0400

    Updating to Python 3 and cleanup

--- aolm_code/utilities/my_debug.py
+++ aolm_code/utilities/my_debug.py
@@ -1,13 +1,22 @@
-from backports.shutil_get_terminal_size import get_terminal_size
+# Author: Jonathan Armoza
+# Created: 
+# Purpose: Debug printing functionality utilizing terminal size and colors
+
+# Imports
+
+# Built-ins
+from os import get_terminal_size
+
+
+# Globals
 
 debug_character = "#"
-debug_divider = debug_character * get_terminal_size().__dict__["columns"]
+debug_divider = debug_character * get_terminal_size().columns
 
-
+# Terminal colors
 
 class bcolors:
 
-	
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -17,22 +26,29 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Library functionality
 
-def debug_print(p_string):
-	print "{0}{1}{2}\n{3}{4}{2}".format(bcolors.OKGREEN, p_string, bcolors.ENDC, 
-									    bcolors.OKBLUE, debug_divider)
+def debug_print(p_string, p_text_color=bcolors.OKGREEN, p_divider_color=bcolors.OKBLUE):
+
+    print(f"{p_divider_color}{debug_divider}{bcolors.ENDC}" + \
+          f"{p_text_color}{p_string}{bcolors.ENDC}\n")
+
+# Test script
 
 def main():
 
-	print "{0}I'm a header{1}".format(bcolors.HEADER, bcolors.ENDC)
-	print "{0}I'm blue okay{1}".format(bcolors.OKBLUE, bcolors.ENDC)
-	print "{0}I'm green okay{1}".format(bcolors.OKGREEN, bcolors.ENDC)
-	print "{0}I'm a warning{1}".format(bcolors.WARNING, bcolors.ENDC)
-	print "{0}I'm a failure{1}".format(bcolors.FAIL, bcolors.ENDC)
-	print "{0}I'm bold{1}".format(bcolors.BOLD, bcolors.ENDC)
-	print "{0}I'm underlined{1}".format(bcolors.UNDERLINE, bcolors.ENDC)
+    print("{0}I'm a header{1}".format(bcolors.HEADER, bcolors.ENDC))
+    print("{0}I'm blue okay{1}".format(bcolors.OKBLUE, bcolors.ENDC))
+    print("{0}I'm green okay{1}".format(bcolors.OKGREEN, bcolors.ENDC))
+    print("{0}I'm a warning{1}".format(bcolors.WARNING, bcolors.ENDC))
+    print("{0}I'm a failure{1}".format(bcolors.FAIL, bcolors.ENDC))
+    print("{0}I'm bold{1}".format(bcolors.BOLD, bcolors.ENDC))
+    print("{0}I'm underlined{1}".format(bcolors.UNDERLINE, bcolors.ENDC))
+    
+    debug_print("This is a test debug print")
 
 
 if "__main__" == __name__:
-	main()
+
+    main()
 
