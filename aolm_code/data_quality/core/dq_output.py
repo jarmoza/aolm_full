3cdd351031cf0592f1be23461e934299ea664cad
jarmoza <jarmoza@gmail.com>
2024-07-11 12:52:31 -0400

    Initial data quality output metric and assessment classes

---
+++ aolm_code/data_quality/core/dq_output.py
@@ -0,0 +1,40 @@
+# Author: Jonathan Armoza
+# Created: June 11, 2024
+# Purpose: Basic output module for data quality metrics
+
+class DataQualityOutput:
+
+    def __init__(self, p_parent_metric_id, p_value):
+        
+        self.m_parent_metric_id = p_parent_metric_id
+
+        # Retrieves from master metric instance list
+        self.m_parent_metric_type = DataQualityMetric.get_metric_type(p_parent_metric_id)
+        self.m_value = p_value
+        self.m_data_type = type(p_value)
+
+    @property
+    def data_type(self):
+        return self.m_data_type
+    @property
+    def value(self):
+        return self.m_value
+
+class DataQualityOutputComparer:
+
+    def __init__(self, p_outputs_list):
+
+        self.m_outputs = p_outputs_list
+
+    def make_comparisons(self):
+
+        for index in len(self.m_outputs):
+            for index2 in len(self.m_outputs):
+                # Make comparison ignoring self comparison
+                pass
+
+    @property
+    def comparisons(self):
+        pass
+
+
---
+++ aolm_code/framework/dq_assessment.py
@@ -0,0 +1,7 @@
+# Author: Jonathan Armoza
+# Created: June 11, 2024
+# Purpose: Basic output assessment module for data quality metric outputs
+
+class DataQualityAssessment:
+
+    pass
