5bc97d29953010dc5aba91092ffb38bac288eabb
jarmoza <jarmoza@gmail.com>
2024-08-02 21:39:01 -0400

    Huck Finn DQ base class

---
+++ experiments/chapter1/huckfinn_dataquality.py
@@ -0,0 +1,24 @@
+# Author: Jonathan Armoza
+# Created: July 25, 2024
+# Purpose: Measuring data quality for the Huckleberry Finn data set
+
+# List of data quality metrics to consider
+
+# 1. Consistency
+# 2. Integrity
+
+# Tasks
+
+# 1. Come up with reasonable interpretations/metrics of consistency and integrity for the Huck Finn corpus
+# 2. Understand the state of the Huck Finn corpus
+# - Create a document where the folders/files of the Twain corpus are explained and their provenance properly cited
+# 3. Consider how metrics will apply to either the raw data downloaded or the demarcated data
+# - e.g. Maybe we would prefer to put some dq metrics over the raw data, and for others examining portions of the text
+# itself to use the demarcated data
+# 4. Consider metadata assessments as dq metrics
+
+# Note: Demarcated data consists of texts that have been uniformly subdivided by using uniform headings
+# i.e. Huck Finn being divided by the same chapter name so chapter text can undergo relevant dq metrics
+
+
+### Experiment 1
