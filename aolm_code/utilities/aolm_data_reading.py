abbb7eeeab294b13902775324697157d182a67cf
jarmoza <jarmoza@gmail.com>
2025-01-14 16:18:52 -0500

    Addition of Internet Archive definitions and generalization of Huck Finn reading functions

--- aolm_code/utilities/aolm_data_reading.py
+++ aolm_code/utilities/aolm_data_reading.py
@@ -11,6 +11,7 @@ import os
 
 # Custom
 from definitions import ROOT_DIR
+from ia_huckfinn_reader import IAHuckFinnReader
 from mtpo_huckfinn_reader import MTPOHuckFinnReader
 from pg_huckfinn_reader import PGHuckFinnReader
 
@@ -26,6 +27,12 @@ PG = "pg"
 METADATA = "metadata"
 TXT = "txt"
 
+huckfinn_source_fullnames = {
+
+    IA: "Internet Archive",
+    MTPO: "Mark Twain Project Online",
+    PG: "Project Gutenberg"
+}
 
 # Paths
 
@@ -33,6 +40,7 @@ huckfinn_directories = {
 
     IA: {
         "json": "{0}{1}data{1}twain{1}huckleberry_finn{1}internet_archive{1}txt{1}demarcated{1}complete{1}json{1}".format(ROOT_DIR, os.sep),
+        "metadata": "{0}{1}data{1}twain{1}huckleberry_finn{1}internet_archive{1}metadata{1}".format(ROOT_DIR, os.sep),
         "txt": "{0}{1}data{1}twain{1}huckleberry_finn{1}internet_archive{1}txt{1}demarcated{1}complete{1}txt{1}".format(ROOT_DIR, os.sep),
     },
     MTPO: "{0}{1}data{1}twain{1}huckleberry_finn{1}mtpo{1}".format(ROOT_DIR, os.sep),
@@ -44,6 +52,16 @@ huckfinn_directories = {
 
 huckfinn_edition_names = {
 
+    IA: [
+        "adventureshuckle00twaiiala",
+        "adventuresofhuc00twai",
+        "adventuresofhuck00twai_9",
+        "adventuresofhuck00twaiuoft",
+        "adventuresofhuck1904twai",
+        "cihm_50160",
+        "dli.ernet.14052",
+        "dli.ernet.470159"
+    ],
     PG: [
         "2011-05-03",
         "2016-08-17",
@@ -53,12 +71,14 @@ huckfinn_edition_names = {
 
 huckfinn_text_filepaths = {
 
+    IA: [f"{huckfinn_directories[IA][TXT]}{edition_name}_demarcated.json" for edition_name in huckfinn_edition_names[IA]],
     MTPO: f"{huckfinn_directories[MTPO]}MTDP10000_edited.xml",
     PG: [f"{huckfinn_directories[PG][TXT]}{edition_name}-HuckFinn.json" for edition_name in huckfinn_edition_names[PG]]
 }
 
 huckfinn_metadata_filepaths = {
 
+    IA: [f"{huckfinn_directories[IA][METADATA]}{edition_name}-HuckFinn_metadata.json" for edition_name in huckfinn_edition_names[IA]],
     PG: [f"{huckfinn_directories[PG][METADATA]}{edition_name}-HuckFinn_metadata.json" for edition_name in huckfinn_edition_names[PG]]
 }
 
@@ -70,14 +90,16 @@ def read_marktwain_project_online_text():
     
     return mtpo_reader    
 
-def read_project_gutenberg_metadata(p_edition_filenames=None):
+def read_huckfinn_metadata(p_source_id, p_edition_filenames=None):
 
-    metadata_filepaths = huckfinn_metadata_filepaths[PG]
+    # 1. Determine filepaths to metadata files
+    metadata_filepaths = huckfinn_metadata_filepaths[p_source_id]
     if None != p_edition_filenames:
         metadata_filepaths = []
         for edition_filename in p_edition_filenames:
-            metadata_filepaths.append(f"{huckfinn_directories[PG][METADATA]}{edition_filename}")
+            metadata_filepaths.append(f"{huckfinn_directories[p_source_id][METADATA]}{edition_filename}")
 
+    # 2. Read in metadata on each <p_source_id> edition of Huckleberry Finn
     metadata_json = {}
     for subject_filepath in metadata_filepaths:
         with open(subject_filepath, "r") as json_file:
@@ -85,19 +107,23 @@ def read_project_gutenberg_metadata(p_edition_filenames=None):
 
     return metadata_json
 
-def read_project_gutenburg_text(p_edition_filenames=None):
+def read_huckfinn_text(p_source_id, p_edition_filenames=None):
 
     huckfinn_text_readers = {}
-    text_filepaths = huckfinn_text_filepaths[PG]
+
+    # 1. Determine filepaths to text files
+    text_filepaths = huckfinn_text_filepaths[p_source_id]
     if None != p_edition_filenames:
         text_filepaths = []
         for edition_filename in p_edition_filenames:
-            text_filepaths.append(f"{huckfinn_directories[PG][TXT]}{edition_filename}")
+            text_filepaths.append(f"{huckfinn_directories[p_source_id][TXT]}{edition_filename}")
 
-
-    # 1. Read in each Project Gutenberg edition of Huckleberry Finn
+    # 2. Read in each <p_source_id> edition of Huckleberry Finn
     for filepath in text_filepaths:
-        huckfinn_text_readers[os.path.basename(filepath)] = PGHuckFinnReader(filepath)
+        if PG == p_source_id:
+            huckfinn_text_readers[os.path.basename(filepath)] = PGHuckFinnReader(filepath)
+        else: # IA == p_source_id
+            huckfinn_text_readers[os.path.basename(filepath)] = IAHuckFinnReader(filepath)
         huckfinn_text_readers[os.path.basename(filepath)].read()
 
     return huckfinn_text_readers
