628bd138f53b771b9fab7400889a91259bb87b35
jarmoza <jarmoza@gmail.com>
2025-01-17 14:01:55 -0500

    New writer object to translate IA/PG style txt files of HuckFinn to json format

---
+++ aolm_code/data_quality/twain/internet_archive/ia_huckfinn_writer.py
@@ -0,0 +1,125 @@
+# Author: Jonathan Armoza
+# Created: January 17, 2025
+# Purpose: Reads a text file and produces a demarcated json for the text
+
+# Imports
+
+# Built-ins
+import json
+import os
+import sys
+
+# Add the project root to sys.path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+sys.path.append(ROOT_DIR)
+from definitions import add_lib_paths
+add_lib_paths()
+
+# Custom
+from aolm_textreader import AOLMTextReader
+
+
+# Classes
+
+class IAHuckFinnWriter:
+
+    # Constructor
+
+    def __init__(self, p_text_filepath, p_json_filepath):
+
+        self.m_text_filepath = p_text_filepath
+        self.m_json_filepath = p_json_filepath
+
+        self.m_textreader = AOLMTextReader(self.m_text_filepath)
+        self.m_textreader.read()
+        with open(self.m_json_filepath, "r") as input_file:
+            self.m_json_template = json.load(input_file)
+
+    # Public methods
+
+    def populate_json_components(self, p_chapter_prefix=""):
+
+        components_json = {}
+
+        for component in self.m_json_template["keys"]["order"]:
+
+            # If component has subcomponents it will be dict with subcomponents, each an array of lines
+            if "subcomponents" in self.m_json_template["keys"]["input"][component] and \
+                self.m_json_template["keys"]["input"][component]["subcomponents"] == "True":
+
+                components_json[component] = {}
+
+                startline = self.m_json_template["keys"]["input"][component]["startline"].strip()
+                endline = self.m_json_template["keys"]["input"][component]["endline"].strip()
+                capturing = False
+                current_chapter = None
+                frontmatter_endline_seen = False
+                frontmatter_endline = self.m_json_template["keys"]["input"]["frontmatter"]["endline"]
+
+                for line in self.m_textreader.text_as_lines:
+
+                    if frontmatter_endline.strip() == line.strip():
+                        frontmatter_endline_seen = True
+                    if not frontmatter_endline_seen:
+                        continue
+
+                    # 1. Read until we find startline
+                    if startline == line.strip():
+                        capturing = True
+
+                    if capturing:
+                        # 2. Read chapters - new chapter each time a "CHAPTER" prefix line is found
+                        if line.strip().startswith("CHAPTER"):
+                            current_chapter = p_chapter_prefix + line.strip()
+                            components_json[component][current_chapter] = []
+
+                        if current_chapter:
+                            components_json[component][current_chapter].append(line.strip())
+
+                        # 3. Stop saving once endline is found
+                        if endline == line.strip():
+                            capturing = False
+                            break
+            
+            # Else, components is just an array of lines
+            else:
+
+                # Save from startline to endline as this component
+                startline = self.m_json_template["keys"]["input"][component]["startline"].strip()
+                endline = self.m_json_template["keys"]["input"][component]["endline"].strip()
+                capturing = False
+                captured_lines = []
+
+                for line in self.m_textreader.text_as_lines:
+                    if startline == line.strip():
+                        capturing = True
+                    if capturing:
+                        captured_lines.append(line.strip())
+                    if endline == line.strip():
+                        capturing = False
+                        break
+
+                    components_json[component] = captured_lines           
+
+        # Reread original json file
+        with open(self.m_json_filepath, "r") as input_file:
+            self.m_json_template = json.load(input_file)
+        
+        # Add new components json to json from file and write to disk
+        self.m_json_template["components"] = components_json
+        with open(self.m_json_filepath, "w") as output_file:
+            json.dump(self.m_json_template, output_file)
+
+
+# Test script
+
+def main():
+
+    huckfinn_txt_file = "/Users/weirdbeard/Documents/school/aolm_full/data/twain/huckleberry_finn/internet_archive/txt/demarcated/incomplete/txt/adventuresofhuck1904twai_demarcated.txt"
+    huckfinn_json_template = "/Users/weirdbeard/Documents/school/aolm_full/data/twain/huckleberry_finn/internet_archive/txt/demarcated/incomplete/json/adventuresofhuck1904twai_demarcated.json"
+    
+    hf_json_writer = IAHuckFinnWriter(huckfinn_txt_file, huckfinn_json_template)
+    hf_json_writer.populate_json_components(p_chapter_prefix="HUCKLEBERRYFINN_BODY_")
+
+if "__main__" == __name__:
+    main()
