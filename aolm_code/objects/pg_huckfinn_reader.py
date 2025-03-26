60324e2555852d3bdb6448962173dfa184fa6c75
jarmoza <jarmoza@gmail.com>
2025-03-26 14:06:13 -0400

    Safety check for existence of chapter in text

--- aolm_code/objects/pg_huckfinn_reader.py
+++ aolm_code/objects/pg_huckfinn_reader.py
@@ -40,7 +40,7 @@ class PGHuckFinnReader(AOLMTextReader):
         roman_numeral = AOLMTextUtilities.roman_numeral_from_decimal(p_chapter_number)
         body_id = f"{self.m_json["keys"]["output"]["body"]}CHAPTER {roman_numeral}."
 
-        return self.m_json["components"]["body"][body_id]
+        return self.m_json["components"]["body"][body_id] if body_id in self.m_json["components"]["body"] else []
     
     def has_chapter(self, p_chapter_number):
 
