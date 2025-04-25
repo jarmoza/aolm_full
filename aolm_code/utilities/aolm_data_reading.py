882676b7b238f45a3fb17da15e34b97bf60e7526
jarmoza <jarmoza@gmail.com>
2025-04-25 12:35:23 -0400

    Adding MTPO to global name/path vars and incorporating MTPO ingestion into read_huckfinn_text

--- aolm_code/utilities/aolm_data_reading.py
+++ aolm_code/utilities/aolm_data_reading.py
@@ -58,7 +58,9 @@ huckfinn_directories = {
         "metadata": "{0}{1}data{1}twain{1}huckleberry_finn{1}internet_archive{1}metadata{1}".format(ROOT_DIR, os.sep),
         "txt": "{0}{1}data{1}twain{1}huckleberry_finn{1}internet_archive{1}txt{1}demarcated{1}complete{1}json{1}".format(ROOT_DIR, os.sep)
     },
-    MTPO: "{0}{1}data{1}twain{1}huckleberry_finn{1}mtpo{1}".format(ROOT_DIR, os.sep),
+    MTPO: {
+        "txt": "{0}{1}data{1}twain{1}huckleberry_finn{1}mtpo{1}".format(ROOT_DIR, os.sep)
+    },
     PG: {
         "metadata": "{0}{1}data{1}twain{1}huckleberry_finn{1}project_gutenberg{1}metadata{1}".format(ROOT_DIR, os.sep),
         "txt": "{0}{1}data{1}twain{1}huckleberry_finn{1}project_gutenberg{1}json{1}".format(ROOT_DIR, os.sep)
@@ -119,7 +121,7 @@ huckfinn_edition_names = {
 huckfinn_text_filepaths = {
 
     IA: [f"{huckfinn_directories[IA][TXT]}{edition_name}-HuckFinn.json" for edition_name in huckfinn_edition_names[IA]],
-    MTPO: f"{huckfinn_directories[MTPO]}MTDP10000_edited.xml",
+    MTPO: [f"{huckfinn_directories[MTPO][TXT]}MTDP10000_edited.xml"],
     PG: [f"{huckfinn_directories[PG][TXT]}{edition_name}-HuckFinn.json" for edition_name in huckfinn_edition_names[PG]]
 }
 
@@ -128,14 +130,7 @@ huckfinn_metadata_filepaths = {
     IA: [f"{huckfinn_directories[IA][METADATA]}{edition_name}-HuckFinn_metadata.json" for edition_name in huckfinn_edition_names[IA]],
     PG: [f"{huckfinn_directories[PG][METADATA]}{edition_name}-HuckFinn_metadata.json" for edition_name in huckfinn_edition_names[PG]]
 }
-
-
-def read_marktwain_project_online_text():
-
-    mtpo_reader = MTPOHuckFinnReader(huckfinn_text_filepaths[MTPO])
-    mtpo_reader.read()
-    
-    return mtpo_reader    
+ 
 
 def read_huckfinn_metadata(p_source_id, p_edition_filenames=None):
 
@@ -157,22 +152,31 @@ def read_huckfinn_metadata(p_source_id, p_edition_filenames=None):
 
 def read_huckfinn_text(p_source_id, p_edition_filenames=None):
 
-    huckfinn_text_readers = {}
+    if p_source_id not in huckfinn_source_fullnames:
+        return None
 
-    # 1. Determine filepaths to text files
-    text_filepaths = huckfinn_text_filepaths[p_source_id]
-    if None != p_edition_filenames:
-        text_filepaths = []
-        for edition_filename in p_edition_filenames:
-            if os.path.exists(f"{huckfinn_directories[p_source_id][TXT]}{edition_filename}"):
-                text_filepaths.append(f"{huckfinn_directories[p_source_id][TXT]}{edition_filename}")
+    if MTPO == p_source_id:
+        mtpo_reader = MTPOHuckFinnReader(huckfinn_text_filepaths[MTPO][0])
+        mtpo_reader.read()
+        return { MTPO: mtpo_reader }
+    else:
+        huckfinn_text_readers = {}
 
-    # 2. Read in each <p_source_id> edition of Huckleberry Finn
-    for filepath in text_filepaths:
-        if PG == p_source_id:
-            huckfinn_text_readers[os.path.basename(filepath)] = PGHuckFinnReader(filepath)
-        else: # IA == p_source_id
-            huckfinn_text_readers[os.path.basename(filepath)] = IAHuckFinnReader(filepath)
-        huckfinn_text_readers[os.path.basename(filepath)].read()
+        # 1. Determine filepaths to text files
+        text_filepaths = huckfinn_text_filepaths[p_source_id]
+        if None != p_edition_filenames:
+            text_filepaths = []
+            for edition_filename in p_edition_filenames:
+                if os.path.exists(f"{huckfinn_directories[p_source_id][TXT]}{edition_filename}"):
+                    text_filepaths.append(f"{huckfinn_directories[p_source_id][TXT]}{edition_filename}")
 
-    return huckfinn_text_readers
+        # 2. Read in each <p_source_id> edition of Huckleberry Finn
+        for filepath in text_filepaths:
+            if PG == p_source_id:
+                huckfinn_text_readers[os.path.basename(filepath)] = PGHuckFinnReader(filepath)
+                huckfinn_text_readers[os.path.basename(filepath)].read()
+            elif IA == p_source_id:
+                huckfinn_text_readers[os.path.basename(filepath)] = IAHuckFinnReader(filepath)
+                huckfinn_text_readers[os.path.basename(filepath)].read()
+                    
+        return huckfinn_text_readers
