027b0059880a3b01d02001f1bc333dd70cf74179
jarmoza <jarmoza@gmail.com>
2025-07-19 13:51:19 -0400

    Hapax compute and evaluate for all Melville novels

--- experiments/chapter1/melville_dataquality_experiment2.py
+++ experiments/chapter1/melville_dataquality_experiment2.py
@@ -20,5 +20,30 @@ add_lib_paths(sys)
 
 import aolm_data_reading
 from aolm_utilities import bar_plot
+from pg_melville_reader import PGMelvilleReader
 from dq_metrics.dataset_signature.hapax_legomenon import DatasetSignature_HapaxLegomenon
+
+# Main script
+
+def main():
+
+    # 0. Setup
+    source_path = aolm_data_reading.melville_source_directory["collected"] + f"demarcated{os.sep}"
+    demarcated_files = [filepath for filepath in glob.glob(source_path + "*.json")]
+
+    # Read each Melville novel into memory
+    readers = [PGMelvilleReader(filepath) for filepath in demarcated_files]
+
+    # Measure hapax by work and by chapter
+    dq_metric = DatasetSignature_HapaxLegomenon(readers)
+    dq_metric.compute()
+    dq_metric.evaluate()
+
+    # Produce comparison visualization
+    if True:
+        return
+
+
+if "__main__" == __name__:
+    main()
 
